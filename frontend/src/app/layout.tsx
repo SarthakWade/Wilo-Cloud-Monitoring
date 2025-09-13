f293c3c0204ed0ccc964ecaef47fd43c945875e0
Sarthak Wadegaonkar <sarthakssg5@gmail.com>
2025-09-13 13:00:09 +0530

    custom favicon added and removed unwanted resources

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Wilo Cloud Monitoring",
   description: "Dashboard for CSV visualization and monitoring",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({
